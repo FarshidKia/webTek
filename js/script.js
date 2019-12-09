71a4983389c942f7b2de2cb10f656201cb1a217d
FarshidKia <ninafarshidkia@gmail.com>
2019-12-10 00:13:25 +0300

    Responsive css
    responsive css ayarlamaları yapıldı

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ $(document).ready(function(){
     $('.special.cards.image').dimmer({
         on:'hover'
     })
+    //sidebar
+    $('.ui.sidebar')
+        .sidebar('attach events','.toc.item');
+    
     //modal
     $('$card1').click(function(){
         $('.ui.modal .modal1').modal('toggle');
